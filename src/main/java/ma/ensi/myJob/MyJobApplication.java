0608a8138d4a59cce7620dd2b1d3ccf3ee12a8ec
siham-benkhajjou <benkhajjousiham@gmail.com>
2025-02-18 19:25:47 +0100

    first creation of the project

---
+++ src/main/java/ma/ensi/myJob/MyJobApplication.java
@@ -0,0 +1,13 @@
+package ma.ensi.myJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ensi/myJob/MyJobApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensi.myJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
